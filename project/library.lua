8989fbc47cd7440adc5ca9c4b4129ce27c09a2a8
Liga <liga@wednesday.wtf>
2022-10-15 17:46:48 -0700

    Logging & interactability with the window.

--- project/library.lua
+++ project/library.lua
@@ -1,25 +1,45 @@
-local NO_DEBUG_OUTPUT = false
+local NO_DEBUG_OUTPUT = true
+local NO_DEBUG_LOG = false
 local THEMES = {
   Dark = {
     primary = "#6a1b9a",
     primary_light = "#9c4dcc",
     primary_dark = "#38006b",
-    secondary = "#212121",
-    secondary_light = "#484848",
-    secondary_dark = "#000000"
+    secondary = "#141414",
+    secondary_light = "#1e1e1e",
+    secondary_dark = "#0c0c0c"
   },
   Light = {
     primary = "#6a1b9a",
     primary_light = "#9c4dcc",
     primary_dark = "#38006b",
-    secondary = "#fafafa",
+    secondary = "#e6e6e6",
     secondary_light = "#ffffff",
     secondary_dark = "#c7c7c7"
   }
 }
+(function()
+  for _, theme in next,THEMES do
+    theme.primary = Color3.fromHex(theme.primary)
+    theme.primary_light = Color3.fromHex(theme.primary_light)
+    theme.primary_dark = Color3.fromHex(theme.primary_dark)
+    theme.secondary = Color3.fromHex(theme.secondary)
+    theme.secondary_light = Color3.fromHex(theme.secondary_light)
+    theme.secondary_dark = Color3.fromHex(theme.secondary_dark)
+  end
+end)()
+local ZINDEX = {
+  ["background"] = 100
+}
+local CURSOR = Vector2.zero
+local EXECUTION_TIMESTAMP = os.time()
+local EXECUTION_DIRECTORY = tostring(os.date("%Y", EXECUTION_TIMESTAMP)) .. "/" .. tostring(os.date("%B", EXECUTION_TIMESTAMP)) .. "/" .. tostring(os.date("%a%d", EXECUTION_TIMESTAMP)) .. ".txt"
+makefolder(tostring(os.date("%Y", EXECUTION_TIMESTAMP)) .. "/")
+makefolder(tostring(os.date("%Y", EXECUTION_TIMESTAMP)) .. "/" .. tostring(os.date("%B", EXECUTION_TIMESTAMP)) .. "/")
+writefile(EXECUTION_DIRECTORY, "Execution Log for " .. tostring(os.date("%a %d %X", EXECUTION_TIMESTAMP)) .. "\n")
 local services = setmetatable({ }, {
   __index = function(self, key)
-    if not (cache) then
+    if not (rawget(self, cache)) then
       self.cache = { }
     end
     if not (self.cache[key]) then
@@ -33,24 +53,102 @@ local constants = {
   camera = services["Workspace"].CurrentCamera
 }
 local log
-log = function(message, ...)
+log = function(message)
+  debug.profilebegin("::log")
   if not (NO_DEBUG_OUTPUT) then
-    return warn(message, ...)
+    services["TestService"]:Message(message)
   end
+  if not (NO_DEBUG_LOG) then
+    appendfile(EXECUTION_DIRECTORY, "[" .. tostring(os.date("%X", os.time())) .. "] " .. tostring(message) .. "\n")
+  end
+  return debug.profileend()
 end
-local object
+local offset
+offset = function(offset)
+  debug.profilebegin("::offset")
+  CURSOR = CURSOR + offset
+  debug.profileend()
+  return CURSOR
+end
+local guid
+guid = function()
+  debug.profilebegin("::guid")
+  local buffer_guid = services["HttpService"]:GenerateGUID(false)
+  log("Generated GUID: " .. tostring(buffer_guid))
+  debug.profileend()
+  return buffer_guid
+end
+local inside
+inside = function(point, bounds, size, roblox)
+  if roblox == nil then
+    roblox = false
+  end
+  if typeof(point) == "Vector3" then
+    point = Vector2.new(point.X, point.Y)
+  end
+  if roblox then
+    point = point + services["GuiService"]:GetGuiInset()
+  end
+  return point.X >= bounds.X and point.X <= bounds.X + size.X and point.Y >= bounds.Y and point.Y <= bounds.Y + size.Y
+end
+local drawing
 do
   local _class_0
-  local _base_0 = { }
-  _base_0.__index = _base_0
-  _class_0 = setmetatable({
-    __init = function(self, type, attributes)
+  local _base_0 = {
+    update = function(self, attributes)
       if attributes == nil then
         attributes = { }
       end
+      debug.profilebegin("drawing:update")
+      self.object["Position"] = self.parent.position + self.cursor_offset
+      for attribute, value in next,attributes do
+        self.object[attribute] = value
+      end
+      return debug.profileend()
+    end,
+    interactable = function(self, callback, state)
+      if callback == nil then
+        callback = (function()
+          return assert(false)
+        end)
+      end
+      if state == nil then
+        state = true
+      end
+      debug.profilebegin("drawing:interactable")
+      self.parent.interactable[self.id] = callback
+      if (not state) then
+        self.parent.interactable[self.id] = nil
+      end
+      return debug.profileend()
+    end,
+    destroy = function(self)
+      debug.profilebegin("drawing:destroy")
+      self.object.Remove()
+      self.object = nil
+      self = nil
+      return debug.profileend()
+    end
+  }
+  _base_0.__index = _base_0
+  _class_0 = setmetatable({
+    __init = function(self, parent, type, attributes)
+      if attributes == nil then
+        attributes = { }
+      end
+      debug.profilebegin("drawing:new")
+      self.id = guid()
+      self.parent = parent
+      self.object = Drawing.new(type)
+      self.cursor_offset = CURSOR
+      for attribute, value in next,attributes do
+        self.object[attribute] = value
+      end
+      self.parent.library.objects[self.id] = self
+      return debug.profileend()
     end,
     __base = _base_0,
-    __name = "object"
+    __name = "drawing"
   }, {
     __index = _base_0,
     __call = function(cls, ...)
@@ -60,7 +158,27 @@ do
     end
   })
   _base_0.__class = _class_0
-  object = _class_0
+  drawing = _class_0
+end
+local tab
+do
+  local _class_0
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  _class_0 = setmetatable({
+    __init = function() end,
+    __base = _base_0,
+    __name = "tab"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  tab = _class_0
 end
 local window
 do
@@ -71,8 +189,78 @@ do
     __init = function(self, parent, title, size, position)
       debug.profilebegin("window:new")
       assert(parent)
+      log("Creating new window: " .. tostring(title))
       self.library = parent
-      self.objects = { }
+      self.window = self
+      self.title = title
+      self.size = size
+      self.position = position
+      self.theme = THEMES.Dark
+      self.dragging = false
+      self.dragging_offset = Vector2.zero
+      self.ignore_dragging = false
+      self.objects = {
+        ["background"] = drawing(self, "Square", {
+          ["Visible"] = true,
+          ["ZIndex"] = ZINDEX.background,
+          ["Transparency"] = 1,
+          ["Color"] = self.theme.secondary,
+          ["Thickness"] = 0,
+          ["Size"] = self.size,
+          ["Position"] = position + offset(Vector2.zero),
+          ["Filled"] = true
+        }),
+        ["border"] = drawing(self, "Square", {
+          ["Visible"] = true,
+          ["ZIndex"] = ZINDEX.background,
+          ["Transparency"] = 1,
+          ["Color"] = self.theme.secondary_light,
+          ["Thickness"] = 0,
+          ["Size"] = self.size,
+          ["Position"] = position + offset(Vector2.zero),
+          ["Filled"] = false
+        })
+      }
+      self.tabs = { }
+      local input_began
+      input_began = function(input)
+        debug.profilebegin("window:input_began")
+        if (input.UserInputType == Enum.UserInputType.MouseButton1) then
+          if (inside(input.Position, self.objects["background"].object.Position, self.objects["background"].object.Size, true) and not ignore_dragging) then
+            self.dragging = true
+            self.dragging_offset = Vector2.new(input.Position.X, input.Position.Y)
+          end
+        end
+        return debug.profileend()
+      end
+      local input_changed
+      input_changed = function(input)
+        debug.profilebegin("window:input_changed")
+        if (input.UserInputType == Enum.UserInputType.MouseMovement) then
+          if (self.dragging) then
+            position = Vector2.new(input.Position.X, input.Position.Y)
+            self.position = self.position + (position - self.dragging_offset)
+            self.dragging_offset = position
+            for _, object in next,self.objects do
+              object:update()
+            end
+          end
+        end
+        return debug.profileend()
+      end
+      local input_ended
+      input_ended = function(input)
+        debug.profilebegin("window:input_ended")
+        if (input.UserInputType == Enum.UserInputType.MouseButton1) then
+          self.dragging = false
+          self.dragging_offset = Vector2.zero
+        end
+        return debug.profileend()
+      end
+      self.library.connections[guid()] = services["UserInputService"].InputBegan:Connect(input_began)
+      self.library.connections[guid()] = services["UserInputService"].InputChanged:Connect(input_changed)
+      self.library.connections[guid()] = services["UserInputService"].InputEnded:Connect(input_ended)
+      return debug.profileend()
     end,
     __base = _base_0,
     __name = "window"
@@ -102,9 +290,23 @@ do
         position = Vector2.new(0, 0)
       end
       debug.profilebegin("library:new_window")
-      self.objects[#self.objects + 1] = window(self, title, size, position)
+      log("Creating new window...")
+      local window_guid = guid()
+      self.windows[window_guid] = window(self, title, size, position)
       debug.profileend()
-      return self.objects[#self.objects]
+      return self.windows[window_guid]
+    end,
+    unload = function(self)
+      debug.profilebegin("library:unload")
+      log("Unloading library...")
+      for _, connection in next,self.connections do
+        connection:Disconnect()
+      end
+      for _, object in next,self.objects do
+        object:destroy()
+      end
+      getgenv().library = nil
+      return debug.profileend()
     end
   }
   _base_0.__index = _base_0
@@ -112,7 +314,10 @@ do
     __init = function(self)
       debug.setmemorycategory("library")
       debug.profilebegin("library:new")
+      log("Initializing library...")
+      self.windows = { }
       self.objects = { }
+      self.connections = { }
       return debug.profileend()
     end,
     __base = _base_0,
@@ -128,4 +333,10 @@ do
   _base_0.__class = _class_0
   library = _class_0
 end
-return library()
+if getgenv().library then
+  getgenv().library:unload()
+end
+local current_library = library()
+local current_window = current_library:new_window("Test Window", Vector2.new(500, 600), (constants.camera.ViewportSize / 2) - Vector2.new(250, 300))
+getgenv().library = current_library
+return current_library
--- project/library.moon
+++ project/library.moon
@@ -1,29 +1,49 @@
 -- CONSTANTS --
-NO_DEBUG_OUTPUT = false
+NO_DEBUG_OUTPUT = true
+NO_DEBUG_LOG = false
 THEMES = {
     Dark: {
         primary: "#6a1b9a",
         primary_light: "#9c4dcc",
         primary_dark: "#38006b",
-        secondary: "#212121",
-        secondary_light: "#484848",
-        secondary_dark: "#000000"
+        secondary: "#141414",
+        secondary_light: "#1e1e1e",
+        secondary_dark: "#0c0c0c"
     },
     Light: {
         primary: "#6a1b9a",
         primary_light: "#9c4dcc",
         primary_dark: "#38006b",
-        secondary: "#fafafa",
+        secondary: "#e6e6e6",
         secondary_light: "#ffffff",
         secondary_dark: "#c7c7c7"
     },
     -- Append more themes here.
 }
+(->
+    for _, theme in next, THEMES do
+        theme.primary = Color3.fromHex(theme.primary)
+        theme.primary_light = Color3.fromHex(theme.primary_light)
+        theme.primary_dark = Color3.fromHex(theme.primary_dark)
+        theme.secondary = Color3.fromHex(theme.secondary)
+        theme.secondary_light = Color3.fromHex(theme.secondary_light)
+        theme.secondary_dark = Color3.fromHex(theme.secondary_dark)
+)()
+ZINDEX = {
+    ["background"]: 100
+}
+CURSOR = Vector2.zero
+EXECUTION_TIMESTAMP = os.time()
+EXECUTION_DIRECTORY = "#{os.date("%Y", EXECUTION_TIMESTAMP)}/#{os.date("%B", EXECUTION_TIMESTAMP)}/#{os.date("%a%d", EXECUTION_TIMESTAMP)}.txt"
+
+makefolder("#{os.date("%Y", EXECUTION_TIMESTAMP)}/")
+makefolder("#{os.date("%Y", EXECUTION_TIMESTAMP)}/#{os.date("%B", EXECUTION_TIMESTAMP)}/")
+writefile(EXECUTION_DIRECTORY, "Execution Log for #{os.date("%a %d %X", EXECUTION_TIMESTAMP)}\n")
 
 -- GLOBAL VARIABLES --
 services = setmetatable({}, {
     __index: (key) =>
-        @cache = {} unless cache
+        @cache = {} unless rawget(@, cache)
         @cache[key] = game\GetService(key) unless @cache[key]
 
         assert(@cache[key])
@@ -35,14 +55,87 @@ constants = {
     camera: services["Workspace"].CurrentCamera
 }
 
-log = (message, ...) ->
-    warn(message, ...) unless NO_DEBUG_OUTPUT
+log = (message) ->
+    debug.profilebegin("::log")
+
+    services["TestService"]\Message(message) unless NO_DEBUG_OUTPUT
+    appendfile(EXECUTION_DIRECTORY, "[#{os.date("%X", os.time())}] #{message}\n") unless NO_DEBUG_LOG
+
+    debug.profileend()
+
+offset = (offset) ->
+    debug.profilebegin("::offset")
+
+    CURSOR += offset
+
+    debug.profileend()
+
+    return CURSOR
+
+guid = () ->
+    debug.profilebegin("::guid")
+
+    buffer_guid = services["HttpService"]\GenerateGUID(false)
+
+    log("Generated GUID: #{buffer_guid}")
+
+    debug.profileend()
+
+    return buffer_guid
+
+inside = (point, bounds, size, roblox = false) ->
+    point = Vector2.new(point.X, point.Y) if typeof(point) == "Vector3"
+    point += services["GuiService"]\GetGuiInset() if roblox
+
+    return point.X >= bounds.X and point.X <= bounds.X + size.X and point.Y >= bounds.Y and point.Y <= bounds.Y + size.Y
 
 -- CLASSES --
-class object
-    new: (type, attributes = {}) =>
-        
+class drawing
+    new: (parent, type, attributes = {}) =>
+        debug.profilebegin("drawing:new")
 
+        @id = guid()
+        @parent = parent
+        @object = Drawing.new(type) -- Not offically deprecated in synapse v3, but it most likely will be.
+        @cursor_offset = CURSOR
+
+        for attribute, value in next, attributes
+            @object[attribute] = value
+
+        @parent.library.objects[@id] = @
+
+        debug.profileend()
+
+    update: (attributes = {}) =>
+        debug.profilebegin("drawing:update")
+
+        @object["Position"] = @parent.position + @cursor_offset
+
+        for attribute, value in next, attributes
+            @object[attribute] = value
+
+        debug.profileend()
+
+    interactable: (callback = (-> assert(false)), state = true) =>
+        debug.profilebegin("drawing:interactable")
+
+        @parent.interactable[@id] = callback
+
+        if (not state)
+            @parent.interactable[@id] = nil
+        
+        debug.profileend()
+
+    destroy: () =>
+        debug.profilebegin("drawing:destroy")
+
+        @object.Remove()
+        @object = nil
+        @ = nil
+
+        debug.profileend()
+
+class tab
 
 class window
     new: (parent, title, size, position) =>
@@ -50,27 +143,134 @@ class window
 
         assert(parent)
 
+        log("Creating new window: #{title}")
+
         @library = parent
-        @objects = {}
+        @window = @
+
+        @title = title
+        @size = size
+        @position = position
+        @theme = THEMES.Dark
+        
+        @dragging = false
+        @dragging_offset = Vector2.zero
+        @ignore_dragging = false
+        
+        @objects = {
+            ["background"]: drawing(@, "Square", {
+                ["Visible"]: true,
+                ["ZIndex"]: ZINDEX.background,
+                ["Transparency"]: 1,
+                ["Color"]: @theme.secondary,
+                ["Thickness"]: 0,
+                ["Size"]: @size,
+                ["Position"]: position + offset(Vector2.zero),
+                ["Filled"]: true
+            }),
+            ["border"]: drawing(@, "Square", {
+                ["Visible"]: true,
+                ["ZIndex"]: ZINDEX.background,
+                ["Transparency"]: 1,
+                ["Color"]: @theme.secondary_light,
+                ["Thickness"]: 0,
+                ["Size"]: @size,
+                ["Position"]: position + offset(Vector2.zero),
+                ["Filled"]: false
+            })
+        }
+
+        @tabs = {}
+
+        input_began = (input) ->
+            debug.profilebegin("window:input_began")
+
+            if (input.UserInputType == Enum.UserInputType.MouseButton1)
+                if (inside(input.Position, @objects["background"].object.Position, @objects["background"].object.Size, true) and not ignore_dragging)
+                    @dragging = true
+                    @dragging_offset = Vector2.new(input.Position.X, input.Position.Y)
+
+            debug.profileend()
+
+        input_changed = (input) ->
+            debug.profilebegin("window:input_changed")
+
+            if (input.UserInputType == Enum.UserInputType.MouseMovement)
+                if (@dragging)
+                    position = Vector2.new(input.Position.X, input.Position.Y)
+
+                    @position += position - @dragging_offset
+                    @dragging_offset = position
+
+                    for _, object in next, @objects
+                        object\update()
+
+            debug.profileend()
+
+        input_ended = (input) ->
+            debug.profilebegin("window:input_ended")
+
+            if (input.UserInputType == Enum.UserInputType.MouseButton1)
+                @dragging = false
+                @dragging_offset = Vector2.zero
+
+            debug.profileend()
+
+        @library.connections[guid()] = services["UserInputService"].InputBegan\Connect(input_began)
+        @library.connections[guid()] = services["UserInputService"].InputChanged\Connect(input_changed)
+        @library.connections[guid()] = services["UserInputService"].InputEnded\Connect(input_ended)
+
+        debug.profileend()
 
 class library
     new: () =>
         debug.setmemorycategory("library")
         debug.profilebegin("library:new")
 
-        @objects = {}
+        log("Initializing library...")
+
+        @windows = {}
+        @objects = {} -- Global list of all objects. Used for unloading.
+        @connections = {} -- Global list of all connections. Used for unloading.
 
         debug.profileend()
 
     new_window: (title = "Window", size = Vector2.new(500, 600), position = Vector2.new(0, 0)) =>
         debug.profilebegin("library:new_window")
 
-        @objects[#@objects + 1] = window(@, title, size, position)
+        log("Creating new window...")
+
+        window_guid = guid()
+
+        @windows[window_guid] = window(@, title, size, position)
 
         debug.profileend()
 
-        return @objects[#@objects]
+        return @windows[window_guid]
+
+    unload: () =>
+        debug.profilebegin("library:unload")
+
+        log("Unloading library...")
+
+        for _, connection in next, @connections
+            connection\Disconnect()
+
+        for _, object in next, @objects
+            object\destroy()
+
+        getgenv().library = nil
+
+        debug.profileend()
 
 -- MAIN ROUTINE --
 
-return library!
+getgenv().library\unload() if getgenv().library
+
+current_library = library!
+
+current_window = current_library\new_window("Test Window", Vector2.new(500, 600), (constants.camera.ViewportSize / 2) - Vector2.new(250, 300))
+
+getgenv().library = current_library
+
+return current_library
